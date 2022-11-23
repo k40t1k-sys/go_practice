7e95fd25202414adf72bf2f85b7478c9e28fa7b1
k40t1k-sys <abhinavkumar06.ak@gmail.com>
2022-11-24 00:39:35 +0530

    intial commit

---
+++ hello_test.go
@@ -0,0 +1,35 @@
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris", "")
+		want := "Hello, Chris"
+		assertCorrecMessage(t, got, want)
+	})
+	t.Run("say 'Hello, World!' when an empty string is suplied", func(t * testing.T) {
+		got := Hello("", "")
+		want := "Hello, World"
+		assertCorrecMessage(t, got, want)
+	})
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrecMessage(t, got, want)
+	})
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Brenda", "French")
+		want := "Bonjour, Brenda"
+		assertCorrecMessage(t, got, want)
+	})
+}
+
+func assertCorrecMessage(t testing.TB, got, want string) {
+	// t.Helper() is needed to tell the test suite that this method is a helper. By doing this when it fails the line number reported will be in our function call rather than inside our test helper.
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
