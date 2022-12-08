c1386168e830ec1d8a3dcc18557dda1244474e0d
k40t1k-sys <abhinavkumar06.ak@gmail.com>
2022-12-09 02:46:58 +0530

    arrays and iterators

---
+++ iteration/repeat.go
@@ -0,0 +1,10 @@
+package iteration
+
+func Repeat(character string, n int) string {
+	var repeated string
+
+	for i := 0; i < n; i++ {
+		repeated = repeated + character
+	}
+	return repeated
+}
---
+++ iteration/repeat_test.go
@@ -0,0 +1,28 @@
+package iteration
+
+import (
+	"testing"
+	"fmt"
+)
+
+
+func ExampleRepeat() {
+	repeat := Repeat("a", 5)
+	fmt.Println(repeat)
+	// Output: "aaaaa"
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a", 5)
+	}
+}
+
+func TestRepeat(t *testing.T) {
+	repeated := Repeat("a", 5)
+	expected := "aaaaa"
+
+	if repeated != expected {
+		t.Errorf("expected %q but got %q", expected, repeated)
+	}
+}
