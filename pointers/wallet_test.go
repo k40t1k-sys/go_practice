68bf290bb5ada5a0317629ddb65bdadf0ed20272
k40t1k-sys <abhinavkumar06.ak@gmail.com>
2023-01-19 03:02:52 +0530

    pointers

--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -45,18 +45,20 @@ func TestWallet(t *testing.T) {
 	t.Run("deposit", func(t *testing.T) {
 		wallet := Wallet{}
 		wallet.Deposit(Bitcoin(10))
+		
 		assertBalance(t, wallet, Bitcoin(10))
 	})
 
-	t.Run("withdraw", func(t *testing.T) {
+	t.Run("withdraw with funds", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(20)}
 		wallet.Withdraw(Bitcoin(10))
+
+		assertNoError(t, err)
 		assertBalance(t, wallet, Bitcoin(10))
 	})
 
 	t.Run("withdraw insufficient funds", func(t *testing.T) {
-		startingBalance := Bitcoin(20)
-		wallet := Wallet{startingBalance}
+		wallet := Wallet{Bitcoin(20)}
 		err := wallet.Withdraw(Bitcoin(100))
 
 		assertError(t, err, ErrInsufficientFunds)
@@ -73,10 +75,18 @@ func assertBalance (t testing.TB, wallet Wallet, want Bitcoin) {
 	}
 }
 
+func assertNoError(t testing.TB, got error) {
+	t.Helper()
+
+	if got != nil {
+		t.Fatal("got an error but didn't want one")
+	}
+}
+
 func assertError (t testing.TB, got error, want error) {
 	t.Helper()
 	if got == nil {
-		t.Fatal("wanted an error but didn't get one")
+		t.Fatal("didn't get an error but wanted one")
 	}
 
 	if got != want {
