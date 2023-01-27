f2b2f0f9f5e04be9f727dec374fed8647e58e2a7
k40t1k-sys <abhinavkumar06.ak@gmail.com>
2023-01-28 03:42:21 +0530

    edits

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -4,16 +4,26 @@ package main
 
 type Dictionary map[string]string
 
+type DictionaryErr string
+
 const (
 	ErrNotFound = DictionaryErr("could not find the word you were looking for")
 	ErrWordExists = DictionaryErr("cannot add word because it already exists")
 	ErrWordDoesNotExist = DictionaryErr("cannot update word because it does not exist")
 )
 
-type DictionaryErr string
-
 func(e DictionaryErr) Error() string {
 	return string(e)
+}
+
+func(d Dictionary) Search(key string) (string, error) {
+	definition, ok := d[key]
+
+	if !ok {
+		return "", ErrNotFound
+	}
+
+	return definition, nil
 }
 
 func(d Dictionary) Add(word, definition string) error {
@@ -29,16 +39,6 @@ func(d Dictionary) Add(word, definition string) error {
 	}
 
 	return nil
-}
-
-func(d Dictionary) Search(key string) (string, error) {
-	definition, ok := d[key]
-
-	if !ok {
-		return "", ErrNotFound
-	}
-
-	return definition, nil
 }
 
 func(d Dictionary) Update(word, definition string) error {
